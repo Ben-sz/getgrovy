21989006f38c1bde83499e37cb6ea0e9edb40fb8
Ben-sz <sz.bence53@gmail.com>
2020-09-11 10:51:18 +0200

    fixes bug for logout

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -3,7 +3,6 @@ import { CanActivate, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 import { map } from 'rxjs/operators';
 import { UserService } from './user.service';
-import 'rxjs/add/operator/switchMap';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { database } from 'firebase';
@@ -27,13 +26,7 @@ export class AdminAuthGuard implements CanActivate {
  */
 
   canActivate() : Observable<boolean>{
-    
-    return this.auth.user$
-      .switchMap( user => { 
-              return this.userService.get(user.uid).valueChanges();
-            } )
-            .pipe( map( appUser => appUser.isAdmin));
-
+     return this.auth.appUser$.pipe( map( appUser => appUser.isAdmin));
   }
 
 
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,6 +5,10 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from './user.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +16,7 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthService {
   user$ : Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private userService: UserService) {
     this.user$ = afAuth.authState;
 
 
@@ -31,4 +35,12 @@ export class AuthService {
     this.afAuth.signOut();
   }
 
+  get appUser$() : Observable<AppUser> {
+    return this.user$
+    .switchMap( user => { 
+          if (user) return this.userService.get(user.uid).valueChanges();
+          
+          return Observable.of(null);
+        });
+  }
 }
