f2831ba1bd6bfe9b11c2a9ec26d2c555d6f89f65
Ben-sz <sz.bence53@gmail.com>
2020-09-14 15:30:50 +0200

    add grid for products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
 
-  constructor() { }
+  constructor( productService: ProductService) {
+    this.products$ = productService.getAll().snapshotChanges().pipe(
+      map(res => res.map(c => ({ key: c.payload.key, ...c.payload.val() as {}   
+    }))));
 
-  ngOnInit(): void {
-  }
+   }
 
+   
+
+ 
 }
