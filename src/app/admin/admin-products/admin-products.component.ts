9ffb135f5d42c17bb955fba5109923525b85459e
Ben-sz <sz.bence53@gmail.com>
2020-09-14 09:37:32 +0200

    adds product interface

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
 
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import {Product} from '../../models/products';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy{
   products$;
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
     this.products$ = this.productService.getAll().snapshotChanges().pipe(
       map(res => res.map(c => ({ key: c.payload.key, ...c.payload.val() as {}   
     }))));
 
-
-
-    this.products$.subscribe(g => console.log("ggg", g));
-
+    this.products$.subscribe( dat => {this.filteredProducts = this.products = dat; console.log('gg',typeof(this.products), typeof(this.products$))});
    }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string){
+    console.log(query);
+    this.filteredProducts = (query) ?
+    this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+    this.products;
   }
 
 }
---
+++ src/app/models/products.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  key: string;
+  title: string;
+  latinName: string;
+  price: number;
+  category: string;
+  imageUrl: string;
+  
+}
