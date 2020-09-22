6317cb31564b7699792b43f503eff06a148630f1
Ben-sz <sz.bence53@gmail.com>
2020-09-22 11:42:06 +0200

    updates total price

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -8,11 +8,17 @@ export class ShoppingCart {
     for (let productId in itemsMap){
       let item = itemsMap[productId];
     this.items.push(new ShoppingCartItem(item.product, item.quantity));
-  
   }
     
   }
 
+  get totalPrice(){
+      let sum = 0;
+      for (let productId in this.items){
+        sum += this.items[productId].totalPrice;
+    }
+    return sum
+  }
 
 
   get totalItemCount(){
