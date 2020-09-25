2486d35b4e402787c564379eea2af7b94d2dbaec
Ben-sz <sz.bence53@gmail.com>
2020-09-25 10:12:13 +0200

    shipping cart sum added

--- src/app/shopping-cart-summary/shopping-cart-summary.component.ts
+++ src/app/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'shopping-cart-summary',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartSummaryComponent {
 
-  constructor() { }
+  @Input('cart') cart:ShoppingCart;
+
+  constructor() { } 
 
   
 
